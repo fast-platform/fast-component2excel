a68d399d08e1b0c0cb7a7e78007eaab301cd9aa4
Tomas <tcastrog10@gmail.com>
2018-10-04 15:00:37 -0300

    table rows now counted correctly

--- src/util/getRows.js
+++ src/util/getRows.js
@@ -8,9 +8,36 @@ function countColumnObjectRows(columns) {
   return Math.max(...columnRows);
 }
 
+function countTableObjectRows(rows) {
+  console.log('rows', rows);
+
+  const tableRows = [];
+
+  for (const row of rows) {
+    const rowMax = [];
+
+    for (const cell of row) {
+      if (cell.components.length > 0) {
+        rowMax.push(getRows(cell.components, 0));
+      } else {
+        rowMax.push(0);
+      }
+    }
+
+    console.log('row', rowMax);
+
+    tableRows.push(Math.max(...rowMax));
+    console.log('max', tableRows);
+  }
+
+  console.log('reduced sum', tableRows.reduce((a, b) => a + b, 0));
+
+  return tableRows.reduce((a, b) => a + b, 0);
+}
+
 export function getRows(tree, rows) {
   for (const comp of tree) {
-    console.log(comp);
+    console.log(comp.type);
     if (
       (comp.type !== 'datagrid') &
       (comp.type !== 'columns') &
@@ -25,7 +52,8 @@ export function getRows(tree, rows) {
       } else if (comp.type === 'columns') {
         rows = rows + countColumnObjectRows(comp.columns);
       } else if (comp.type === 'table') {
-        rows = rows + comp.numRows;
+        // rows = rows + comp.numRows;
+        rows = rows + countTableObjectRows(comp.rows);
       } else if (comp.type === 'fieldset') {
         rows = getRows(comp.components, rows);
       } else {
