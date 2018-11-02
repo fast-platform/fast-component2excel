047b4c0fb6b814428dce2870791559ad6f2fd7e2
Gabriel <gabriel@garox.org>
2018-11-02 10:45:45 -0300

    fix time

--- src/components/TimeComponent/TimeComponent.js
+++ src/components/TimeComponent/TimeComponent.js
@@ -1,6 +1,14 @@
 import stampit from '@stamp/it';
-import TextFieldComponent from '../TextFieldComponent/TextFieldComponent';
+import NumberComponent from '../NumberComponent/NumberComponent';
 
-export default stampit(TextFieldComponent, {
-
+export default stampit(NumberComponent, {
+  init({component}) {
+    this.format = component.format;
+  },
+  methods: {
+    setFormat() {
+      this.errorLabel = 'Not a time';
+      this.inputField.value(new Date()).style('numberFormat', this.format + ';@');
+    }
+  }
 });
